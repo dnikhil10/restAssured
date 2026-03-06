230f3c636ad72c2a50dccf66298eb2a89b909b8b
nikhil <nikhil.doddawad@gmail.com>
2026-03-06 15:03:52 +0530

    adding changes of the rest assured

---
+++ src/test/java/PoJo/Api.java
@@ -0,0 +1,19 @@
+package PoJo;
+
+public class Api {
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    private String courseTitle;
+    private int price;
+}
