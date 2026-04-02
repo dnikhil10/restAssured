2942f251713513902267b9668367024ec04dc9d0
dnikhil10 <95733290+dnikhil10@users.noreply.github.com>
2026-04-02 10:59:48 +0530

    Update ECommerceAPITest.java

--- src/test/java/Ecommerce/ECommerceAPITest.java
+++ src/test/java/Ecommerce/ECommerceAPITest.java
@@ -52,7 +52,7 @@ public class ECommerceAPITest {
        JsonPath js=new JsonPath(addResponse);
        String productId=js.get("productId");
        System.out.println(productId);
-      System.out.println("Tested and checking for rebase");
+      System.out.println("Tested and checking for rebase and rebased");
    }
 }
 
