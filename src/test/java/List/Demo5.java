9f47fcb394d5380e5799189ece5f0eff1aed85ba
nikhil <nikhil.doddawad@gmail.com>
2026-04-09 20:09:17 +0530

    Adding Demo5 class with @Test

--- src/test/java/List/Demo5.java
+++ src/test/java/List/Demo5.java
@@ -1,11 +1,14 @@
 package List;
+import org.testng.annotations.Test;
 
-public class Demo5
-  {
 
-    public static void main (String[] args)
+public class Demo5 {
+
+    @Test
+    public void test()
     {
+        System.out.println("Testing");
         System.out.println("Hello World");
     }
-    
-  }
+}
+
